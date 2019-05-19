f01c8da16ecd97fbb7f62abdfa4082f4328bb2aa
Francisco Chaves <francisco.jschaves@gmail.com>
2019-05-18 22:48:24 -0300

    Testar mensagem do console móvel

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 'use strict'
 
-console.log('Here\'s a hidden message');
-
 let today  = new Date();
 let formatDate = today.toLocaleString();
 let selectElement = document.getElementById('date');
 selectElement.innerHTML = formatDate;
+
+console.log('Here\'s a hidden message');
